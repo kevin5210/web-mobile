df5bad071a2b67ba8c0840e84f0a6af25bee80c2
kevin5210 <bida5210@163.com>
2023-06-15 15:25:31 +0800

    ai init

---
+++ assets/main/index.js
@@ -0,0 +1,22 @@
+System.register("chunks:///_virtual/main",["./main.ts"],(function(){"use strict";return{setters:[null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/main.ts",["./rollupPluginModLoBabelHelpers.js","cc"],(function(t){"use strict";var n,e,r,o;return{setters:[function(t){n=t.inheritsLoose},function(t){e=t.cclegacy,r=t._decorator,o=t.Component}],execute:function(){var i;e._RF.push({},"88ee9oPWW1GPLjFisQfDytm","main",void 0);var c=r.ccclass;r.property,t("main",c("main")(i=function(t){function e(){return t.apply(this,arguments)||this}n(e,t);var r=e.prototype;return r.start=function(){},r.update=function(t){},e}(o))||i);e._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/main', 'chunks:///_virtual/main'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
